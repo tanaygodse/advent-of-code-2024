c42625b7ea2ed093da8968a7656dac760b777078
Tanay Godse <tanaygodse@gmail.com>
2024-12-08 20:25:59 -0800

    Day 8 Refactor

--- day-08/8a.py
+++ day-08/8a.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 with open("in") as f:
     lines = f.read().splitlines()
 
-print(lines)
 
 rows = len(lines)
 cols = len(lines[0])
@@ -17,65 +16,31 @@ for i in range(rows):
         if lines[i][j] != '.':
             distances[lines[i][j]].append((i, j))
 
-print(distances)
 for k, v in distances.items():
-    print(k)
     for i, v1 in enumerate(v):
         x1, y1 = v1
-        print("x1, y1", x1, y1)
         for j, v2 in enumerate(v[i+1:]):
             x2, y2 = v2
-            print("x2, y2", x2, y2)
-            if x2 > x1:
-                if y2 > y1:
-                    dx = x2 - x1
-                    dy = y2 - y1
-                    nx = x1 - dx
-                    ny = y1 - dy
-                    print(nx, ny)
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
-                    nx = x2 + dx
-                    ny = y2 + dy
-                    print(nx, ny)
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
-                else:
-                    dx = x2 - x1
-                    dy = y1 - y2
-                    nx = x1 - dx
-                    ny = y1 + dy
-                    print(nx, ny)
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
-                    nx = x2 + dx
-                    ny = y2 - dy
-                    print(nx, ny)
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
+            if y2 > y1:
+                dx = x2 - x1
+                dy = y2 - y1
+                nx = x1 - dx
+                ny = y1 - dy
+                if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
+                    placed.add((nx, ny))
+                nx = x2 + dx
+                ny = y2 + dy
+                if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
+                    placed.add((nx, ny))
             else:
-                if y2 > y1:
-                    dx = x1 - x2
-                    dy = y2 - y1
-                    nx = x1 + dx
-                    ny = y1 - dy
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
-                    nx = x2 - dx
-                    ny = y2 + dy
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
-                else:
-                    dx = x1 - x2
-                    dy = y1 - y2
-                    nx = x1 + dx
-                    ny = y1 + dy
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
-                    nx = x2 - dx
-                    ny = y2 - dy
-                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                        placed.add((nx, ny))
-            print('------')
-print(sorted([(x, y) for x, y in placed]))
+                dx = x2 - x1
+                dy = y1 - y2
+                nx = x1 - dx
+                ny = y1 + dy
+                if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
+                    placed.add((nx, ny))
+                nx = x2 + dx
+                ny = y2 - dy
+                if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
+                    placed.add((nx, ny))
 print(len(placed))
--- day-08/8b.py
+++ day-08/8b.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 with open("in") as f:
     lines = f.read().splitlines()
 
-print(lines)
 
 rows = len(lines)
 cols = len(lines[0])
@@ -17,89 +16,45 @@ for i in range(rows):
         if lines[i][j] != '.':
             distances[lines[i][j]].append((i, j))
 
-print(distances)
 for k, v in distances.items():
-    print(k)
     for i, v1 in enumerate(v):
         x1, y1 = v1
         placed.add((x1, y1))
-        print("x1, y1", x1, y1)
         for j, v2 in enumerate(v[i+1:]):
             x2, y2 = v2
             placed.add((x2, y2))
-            print("x2, y2", x2, y2)
-            if x2 > x1:
-                if y2 > y1:
-                    dx = x2 - x1
-                    dy = y2 - y1
-                    nx = x1 - dx
-                    ny = y1 - dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        print(nx, ny)
-                        if (nx, ny) not in placed:
-                            placed.add((nx, ny))
-                        nx -= dx
-                        ny -= dy
-                    nx = x2 + dx
-                    ny = y2 + dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        print(nx, ny)
-                        if (nx, ny) not in placed:
-                            placed.add((nx, ny))
-                        nx += dx
-                        ny += dy
-                else:
-                    dx = x2 - x1
-                    dy = y1 - y2
-                    nx = x1 - dx
-                    ny = y1 + dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        print(nx, ny)
-                        if (nx, ny) not in placed:
-                            placed.add((nx, ny))
-                        nx -= dx
-                        ny += dy
-                    nx = x2 + dx
-                    ny = y2 - dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        print(nx, ny)
-                        if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
-                            placed.add((nx, ny))
-                        nx += dx
-                        ny -= dy
+            if y2 > y1:
+                dx = x2 - x1
+                dy = y2 - y1
+                nx = x1 - dx
+                ny = y1 - dy
+                while 0 <= nx < rows and 0 <= ny < cols:
+                    if (nx, ny) not in placed:
+                        placed.add((nx, ny))
+                    nx -= dx
+                    ny -= dy
+                nx = x2 + dx
+                ny = y2 + dy
+                while 0 <= nx < rows and 0 <= ny < cols:
+                    if (nx, ny) not in placed:
+                        placed.add((nx, ny))
+                    nx += dx
+                    ny += dy
             else:
-                if y2 > y1:
-                    dx = x1 - x2
-                    dy = y2 - y1
-                    nx = x1 + dx
-                    ny = y1 - dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        if (nx, ny) not in placed:
-                            placed.add((nx, ny))
-                        nx += dx
-                        ny -= dy
-                    nx = x2 - dx
-                    ny = y2 + dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        if (nx, ny) not in placed:
-                            placed.add((nx, ny))
-                        nx -= dx
-                        ny += dy
-                else:
-                    dx = x1 - x2
-                    dy = y1 - y2
-                    nx = x1 + dx
-                    ny = y1 + dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        if (nx, ny) not in placed:
-                            placed.add((nx, ny))
-                        nx += dx
-                        ny += dy
-                    nx = x2 - dx
-                    ny = y2 - dy
-                    while 0 <= nx < rows and 0 <= ny < cols:
-                        if (nx, ny) not in placed:
-                            placed.add((nx, ny))
-            print('------')
-print(sorted([(x, y) for x, y in placed]))
+                dx = x2 - x1
+                dy = y1 - y2
+                nx = x1 - dx
+                ny = y1 + dy
+                while 0 <= nx < rows and 0 <= ny < cols:
+                    if (nx, ny) not in placed:
+                        placed.add((nx, ny))
+                    nx -= dx
+                    ny += dy
+                nx = x2 + dx
+                ny = y2 - dy
+                while 0 <= nx < rows and 0 <= ny < cols:
+                    if (nx, ny) not in placed and 0 <= nx < rows and 0 <= ny < cols:
+                        placed.add((nx, ny))
+                    nx += dx
+                    ny -= dy
 print(len(placed))
