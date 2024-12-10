5c1f4c5300a4176cd363cced1b14154008a0171a
Tanay Godse <tanaygodse@gmail.com>
2024-12-09 22:16:03 -0800

    Day 10

---
+++ day-10/10b.py
@@ -0,0 +1,39 @@
+with open('in') as f:
+    lines = f.read().splitlines()
+
+print(lines)
+
+adj_matrix = []
+
+for l in lines:
+    adj_matrix.append([int(i) for i in l])
+
+print(adj_matrix)
+
+rows = len(adj_matrix)
+cols = len(adj_matrix[0])
+
+dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+
+def dfs(r, c, val):
+    if adj_matrix[r][c] != val:
+        return 0
+    if val == 9:
+        return 1
+    res = 0
+    for dr, dc in dirs:
+        nr = r + dr
+        nc = c + dc
+        if 0 <= nr < rows and 0 <= nc < cols and adj_matrix[nr][nc] == val + 1:
+            res += dfs(nr, nc, val+1)
+    return res
+
+
+paths = 0
+for r in range(rows):
+    for c in range(cols):
+        if adj_matrix[r][c] == 0:
+            paths += dfs(r, c, adj_matrix[r][c])
+
+print(paths)
